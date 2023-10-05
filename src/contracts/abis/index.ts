de1305975cce0b80b371fa15d59dfed3e34c9b43
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-10-05 19:27:01 +0100

    chore: add abis and addresses

---
+++ src/contracts/abis/index.ts
@@ -0,0 +1,7 @@
+export default {
+  USDCAdapterAbi: require('./USDCAdapter.json'),
+  CoreVaultAbi: require('./CoreVault.json'),
+  USDCAbi: require('./USDC.json'),
+  xNGNAbi: require('./xNGN.json'),
+  xNGNAdapterAbi: require('./xNGNAdapter.json'),
+};
