babd67f4de3623bb5b76e5c30553c58ebd21ead8
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-10-06 01:17:47 +0100

    chore: implement vault getter methods

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,18 @@ import { ICollateral, IContract, IProvider, ISigner } from './types';
 import { Contract } from './libs/contract';
 import Addresses from './contracts/addresses/base.json';
 import abis from './contracts/abis';
+import {
+  getAvailablexNGN,
+  getVaultById,
+  getVaultsForOwner,
+} from './services/vault';
 
 export class DescentClass {
   protected signer: ISigner;
   protected provider: IProvider;
-  private vaultContract?: IContract;
-  private usdcAdapterContract?: IContract;
-  private xNGNAdapterContract?: IContract;
+  private vaultContract: IContract;
+  private usdcAdapterContract: IContract;
+  private xNGNAdapterContract: IContract;
 
   constructor(signer: ISigner, provider: IProvider) {
     this.provider = provider;
@@ -36,28 +41,34 @@ export class DescentClass {
    * @param vaultId Vault ID
    * @returns The Vault information
    */
-  public getVault(vaultId: string) {}
+  public async getVault(vaultId: string) {
+    const vault = await getVaultById(vaultId, this.vaultContract);
+    return vault;
+  }
 
   /**
    * @dev Gets all the vaults associated with an address
    * @param ownerAddress Vault owner address
    * @returns List of vaults
    */
-  public getVaultsByOwner(ownerAddress: string) {}
+  public async getVaultsByOwner(ownerAddress: string) {
+    const vaults = await getVaultsForOwner(ownerAddress, this.vaultContract);
 
-  /**
-   * @dev Gets all the vault Ids tied to an address
-   * @param ownerAddress Vault owner address
-   * @returns List of vault IDs
-   */
-  public getVaultIDs(ownerAddress: string) {}
+    return vaults;
+  }
 
   /**
    * @dev Gets the amount of xNGN available for an owner to mint
    * @param ownerAddress Owner of the vault
    * @returns Amount of xNGN available
    */
-  public getTotalxNGNAvailableForOwner(ownerAddress: string) {}
+  public async getTotalxNGNAvailableForOwner(ownerAddress: string) {
+    const availablexNGN = await getAvailablexNGN(
+      ownerAddress,
+      this.vaultContract
+    );
+    return availablexNGN;
+  }
 
   /**
    * @dev Creates a new vault
@@ -65,7 +76,7 @@ export class DescentClass {
    * @param collateralName name of the collateral to be created for vault
    * @returns vault Id
    */
-  public createVault(collateralName: ICollateral) {}
+  public async createVault(collateralName: ICollateral) {}
 
   /**
    * @dev lock usdc for a particular vault
@@ -73,7 +84,7 @@ export class DescentClass {
    * @param vaultID vault id to lock usdc for
    * @returns lockedCollateral
    */
-  public lockUSDC(collateralAmount: string | number, vaultID: string) {}
+  public async lockUSDC(collateralAmount: string | number, vaultID: string) {}
 
   /**
    * @dev mint available xNGN a particular vault
@@ -81,7 +92,7 @@ export class DescentClass {
    * @param vaultID vault id to mint xNGN for
    * @returns available xNGN
    */
-  public mintAvailablexNGN(amount: string | number, vaultID: string) {}
+  public async mintAvailablexNGN(amount: string | number, vaultID: string) {}
 
   /**
    * @dev repay borrowed xNGN for a particular vault
@@ -89,7 +100,7 @@ export class DescentClass {
    * @param vaultID vault id to repay xNGN for
    * @returns vaultDebt
    */
-  public repayxNGN(amount: string | number, vaultID: string) {}
+  public async repayxNGN(amount: string | number, vaultID: string) {}
 
   /**
    * @dev withdraw usdc for a particular vault
@@ -97,7 +108,10 @@ export class DescentClass {
    * @param vaultID vault id to withdraw usdc from
    * @returns unlockedCollateral
    */
-  public withdrawUSDC(collateralAmount: string | number, vaultID: string) {}
+  public async withdrawUSDC(
+    collateralAmount: string | number,
+    vaultID: string
+  ) {}
 
   /**
    * @dev Creates a new vault and lock usdc in one transaction
@@ -105,7 +119,7 @@ export class DescentClass {
    * @param collateralAmount amount of collateral to lock for vault
    * @returns vault Id
    */
-  public createVaultandLockUSDC(
+  public async createVaultandLockUSDC(
     collateralName: ICollateral,
     collateralAmount: string | number
   ) {}
@@ -116,7 +130,7 @@ export class DescentClass {
    * @param collateralAmount amount of collateral to lock for vault
    * @returns vault Id, locked usdc & minted xngn
    */
-  public createVaultLockUSDCandMintxNGN(
+  public async createVaultLockUSDCandMintxNGN(
     collateralName: ICollateral,
     collateralAmount: string | number
   ) {}
--- src/services/vault.ts
+++ src/services/vault.ts
@@ -6,13 +6,18 @@ const getVaultById = async (vaultId: string | number, contract: IContract) => {
     const vaultData = await contract.getVaultById(vaultId.toString());
 
     let vaultState;
-    if (ethers.formatUnits(vaultData[4].vaultState) == '0') {
-      vaultState = 'Idle';
-    } else if (ethers.formatUnits(vaultData[4].vaultState) == '1') {
-      vaultState = 'Active';
-    } else {
-      vaultState = 'Inactive';
+    switch (ethers.formatUnits(vaultData[4].vaultState)) {
+      case '0':
+        vaultState = 'Idle';
+        break;
+      case '1':
+        vaultState = 'Active';
+        break;
+      default:
+        vaultState = 'Inactive';
+        break;
     }
+
     return {
       lockedCollateral: ethers.formatUnits(vaultData[0].lockedCollateral),
       unlockedCollateral: ethers.formatUnits(vaultData[1].unlockedCollateral),
@@ -26,4 +31,53 @@ const getVaultById = async (vaultId: string | number, contract: IContract) => {
   }
 };
 
-export { getVaultById };
+const getVaultsForOwner = async (owner: string, contract: IContract) => {
+  try {
+    const vaults: any = [];
+
+    const vaultIds: [] = await contract.getVaultsForOwner(owner);
+    await Promise.all(
+      vaultIds.map(async vaultId => {
+        const vault = await contract.getVaultById(vaultId);
+
+        let vaultState;
+        switch (ethers.formatUnits(vault[4].vaultState)) {
+          case '0':
+            vaultState = 'Idle';
+            break;
+          case '1':
+            vaultState = 'Active';
+            break;
+          default:
+            vaultState = 'Inactive';
+            break;
+        }
+
+        vaults.push({
+          vaultId: vaultId,
+          lockedCollateral: ethers.formatUnits(vault[0].lockedCollateral),
+          unlockedCollateral: ethers.formatUnits(vault[1].unlockedCollateral),
+          normalisedDebt: ethers.formatUnits(vault[2].normalisedDebt),
+          collateralName: ethers.decodeBytes32String(vault[3].normalisedDebt),
+          vaultState: vaultState,
+        });
+      })
+    );
+    return vaults;
+  } catch (e) {
+    const message = await ErrorMessage(e);
+    return message;
+  }
+};
+
+const getAvailablexNGN = async (owner: string, contract: IContract) => {
+  try {
+    const availablexNGN = await contract.getAvailableStableToken(owner);
+    return Number(ethers.formatUnits(availablexNGN, 18));
+  } catch (e) {
+    const message = await ErrorMessage(e);
+    return message;
+  }
+};
+
+export { getVaultById, getVaultsForOwner, getAvailablexNGN };
