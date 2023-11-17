8ddc259f75457e35373a5a9bb463bd29b46cb0c3
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-11-17 13:51:20 +0100

    chore: add contract addresses

--- src/contracts/getContractAddresses.ts
+++ src/contracts/getContractAddresses.ts
@@ -5,22 +5,22 @@ const addresses: Record<
   Partial<Record<SupportedNetworkId, string>> | string
 > = {
   Vault: {
-    '84531': '',
+    '84531': '0xeAb261C2021Af0e3AC9D716C6b7BaDAd73caCfff',
   },
   MultiStaticcall: {
-    '84531': '',
+    '84531': '0x5d0583Ef20884C0b175046d515Ec227200C12C89',
   },
   VaultGetters: {
-    '84531': '',
+    '84531': '0x18196CCaA8C2844c82B40a8bDCa27349C7466280',
   },
   VaultRouters: {
-    '84531': '',
+    '84531': '0x94D80B2EA3cda86bF350DD7860e1171701F284c8',
   },
   Currency: {
-    '84531': '',
+    '84531': '0x774843f6Baa4AAE62F026a8aF3c1C6FF3e55Ca39',
   },
   USDC: {
-    '84531': '',
+    '84531': '0xF175520C52418dfE19C8098071a252da48Cd1C19',
   },
 };
 
