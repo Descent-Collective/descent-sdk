ed9dd4b148e649c7443370eb260645462c8f60e9
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-11-28 07:41:14 +0100

    chore: add typechain to generate contract typings

---
+++ scripts/runTypechain.ts
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import fsp from 'fs/promises';
+import path from 'path';
+import { glob, runTypeChain } from 'typechain';
+
+async function main() {
+  const cwd = process.cwd();
+
+  if (!fs.existsSync('./cache/json-abis'))
+    await fsp.mkdir('./cache/json-abis', { recursive: true });
+
+  const importABIs = glob(cwd, [
+    '../src/contracts/abis/MultiStaticcall.json',
+    '../src/contracts/abis/usdc.json',
+    '../src/contracts/abis/Vault.json',
+    '../src/contracts/abis/VaultGetters.json',
+    '../src/contracts/abis/VaultRouter.json',
+    '../src/contracts/abis/Currency.json',
+  ]);
+
+  const copiedABIs: string[] = [];
+
+  for (const file of importABIs) {
+    const dest = `${cwd}/cache/json-abis/${file.split('/').pop()}`;
+
+    const content = await fsp.readFile(file, 'utf8');
+
+    const obj = JSON.parse(content) as Record<string, any>;
+    obj.bytecode = '0x';
+    obj.deployedBytecode = '0x';
+    await fsp.writeFile(dest, JSON.stringify(obj));
+
+    copiedABIs.push(dest);
+  }
+
+  await runTypeChain({
+    cwd,
+    filesToProcess: copiedABIs,
+    allFiles: copiedABIs,
+    outDir: './src/generated',
+    target: 'ethers-v5',
+  });
+}
+
+main()
+  .catch(console.error)
+  .finally(async () => {
+    if (fs.existsSync('./cache/json-abis'))
+      await fsp.rm('./cache/json-abis', { recursive: true, force: true });
+  });
--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -2,6 +2,24 @@ import { ethers } from 'ethers';
 import { ISigner } from '../types';
 
 import { getContractAddress } from './getContractAddresses';
-import { ContractName } from './types';
+import { ContractName, SupportedNetwork } from './types';
 
-export default class ContractManager {}
+export default class ContractManager {
+  private contractAddress: string;
+  public signer: ISigner;
+
+  constructor(
+    public contractName: ContractName,
+    signer: ISigner,
+    public chainId: SupportedNetwork,
+  ) {
+    this.contractName = contractName;
+    this.chainId = chainId;
+    this.contractAddress = (getContractAddress(contractName) || {})[chainId] || '';
+    if (!this.contractAddress) {
+      throw new Error(`No address for contract ${contractName}`);
+    }
+    new ethers.Contract(this.contractAddress, abi, this.signer);
+  }
+    
+}
